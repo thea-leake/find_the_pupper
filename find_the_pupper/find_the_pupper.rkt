a254737a611f89a25af699af56c35c1f2a724f2d
Thea Leake <thea.leake@gmail.com>
2020-09-26 13:40:49 -0700

    Added basic map

--- find_the_pupper/find_the_pupper.rkt
+++ find_the_pupper/find_the_pupper.rkt
@@ -3,6 +3,39 @@
 (require data/collection)
 
 
+(struct map-terrain
+  (terrain-type
+   player-can-occupy? )
+  #:transparent
+  )
+
+(struct location ;; probably won't use this
+  (terrain
+   characters)
+  #:transparent)
+
+(struct character
+  (creature-type
+   is-player?
+   is-lost-pupper?)
+  #:transparent)
+
+(define road (map-terrain "road"
+                          #t))
+
+(define bldg (map-terrain "building"
+                              #f))
+
+(define park (map-terrain "park"
+                          #t))
+
+(define player (character "human"
+                          #t
+                          #f))
+
+(define pupper (character "pupper"
+                          #f
+                          #t))
 (define map-width 16)
 (define map-height 8)
 
@@ -11,52 +44,26 @@
 
 (define map-cell-count (* map-width map-height))
 
-;;rbbbrbbbrbbbrbbb
-;;rbbbrbbbrbbbrbbb
-;;rbbbrbbbrbbbrbbb
-;;rrrrrrrrrrrrrrrr
-;;rbbbrpbbrbbbrbbb
-;;rbbbrppprbbbrbbb
-;;rbbbrpbbrbbbrbbb
-;;rrrrrrrrrrrrrrrr
+(define terrain-map
+  '(
+    road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg
+    road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg
+    road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg road bldg bldg bldg
+    road road road road road road road road road road road road road road road road
+    road bldg park bldg road bldg bldg bldg road bldg park bldg road bldg park bldg
+    road bldg park bldg road bldg bldg bldg road bldg park park road park park bldg
+    road park park park road bldg bldg bldg road bldg park bldg road bldg park bldg
+    road road road road road road road road road road road road road road road road
+    road bldg bldg bldg road bldg bldg bldg road park park park road bldg bldg bldg
+    road bldg bldg bldg road bldg bldg bldg road park park park road bldg bldg bldg
+    road bldg bldg bldg road bldg bldg bldg road bldg park bldg road bldg bldg bldg
+    road road road road road road road road road road road road road road road road
+         ))
 
+(define (gen-init-location terrain)
+  (location terrain '[]))
 
+(define init-map (apply vector-immutable
+                        (map gen-init-location terrain-map)))
 
-(struct map-terrain
-  (terrain-type
-   player-can-occupy? )
-  #:transparent
-  )
-
-(struct location ;; probably won't use this
-  (index
-   terrain
-   characters)
-  #:transparent)
-
-(struct character
-  (index
-   creature-type
-   is-player?
-   is-lost-pupper?)
-  #:transparent)
-
-(define road (map-terrain "road"
-                          #t))
-
-(define building (map-terrain "building"
-                              #f))
-
-(define park (map-terrain "park"
-                          #t))
-
-(define player (character 0
-                          "human"
-                          #t
-                          #f))
-
-(define pupper (character (- map-cell-count 1)
-                          "pupper"
-                          #f
-                          #t))
 
