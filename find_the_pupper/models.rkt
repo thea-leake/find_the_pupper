b9646c93d8c3431a8a71050ea5856d523bcfac21
Thea Leake <thea.leake@gmail.com>
2020-09-27 01:26:50 -0700

    Fixed move check fns so they work for all dimentions

--- find_the_pupper/models.rkt
+++ find_the_pupper/models.rkt
@@ -79,46 +79,57 @@
     road road road road road road road road road road road road road road road road
          ))
 
+
 (define (gen-init-location terrain)
   (location terrain (set)))
 
+
 (define init-map (apply vector-immutable
                         (map gen-init-location terrain-map)))
+
 
 (define (toggle-player player)
   (if (eq? player human)
       pupper
       human))
 
+
 (define (toggle-player-turn turn)
   (struct-copy gamestate turn
                [current-player
                 (toggle-player (gamestate-current-player turn))]))
+
 
 (define (get-row current-map row)
   (subsequence current-map
                (* (- row 1) map-width)
                (* row map-width)))
 
+
 (define (get-column current-map row)
   (apply vector-immutable
          (map (lambda (x) (nth current-map (+ row x)))
               (range map-height))))
 
+
 (define (in-range? index)
   (and (< index map-cell-count)
        (>= index 0)))
 
+
 (define (index-row index)
   (quotient index map-width))
 
+
 (define (index-column index)
   (remainder index map-width))
+
 
 (define (same-axis? fn)
   (lambda (index1 index2)
     (= (fn index1)
        (fn index2))))
+
 
 (define same-row? (same-axis? index-row))
 (define same-column? (same-axis? index-column))
@@ -129,13 +140,16 @@
     (and (in-range? to)
          (check-fn from to))))
 
+
 (define horizontal-move-check (axis-move-check same-row?))
 (define vertical-move-check (axis-move-check same-column?))
+
 
 (define (location-occupiable? current-map index)
   (map-terrain-enerable?
    (location-terrain
     (nth current-map index))))
+
 
 ;; depending on what's provided out this will probably be redundant
 (define (move-1-away? from to)
@@ -144,20 +158,20 @@
       (= map-width
          (abs (- from to)))))
 
-(define (map-axis-check check-fn)
-  (lambda (current-map from to)
-    (and (check-fn from to)
-         (move-1-away? from to)
-         (location-occupiable? current-map to))))
 
-(define valid-horizontal-move (map-axis-check horizontal-move-check))
-(define valid-vertical-move (map-axis-check vertical-move-check))
+(define (valid-move current-map from to)
+  (and (or (horizontal-move-check from to)
+           (vertical-move-check from to))
+   (move-1-away? from to)
+   (location-occupiable? current-map to)))
+
 
 (define (get-player-location turn)
   (let ([player (gamestate-current-player turn)])
     (match player
       [human  (gamestate-human-location turn)]
       [pupper (gamestate-pupper-location turn)])))
+
 
 (define (update-player-location turn to)
   (let ([player (gamestate-current-player turn)])
@@ -167,6 +181,7 @@
       [pupper (struct-copy gamestate turn
                            [pupper-location to])])))
 
+
 (define (move-updated turn to)
   (turnresponse (toggle-player-turn
                  (update-player-location turn to))
@@ -175,23 +190,25 @@
 (define (move-failed turn)
   (turnresponse turn
                 #f
-                "Move invalid"))
+                "You can't move there"))
 
 (define (move-player move-fn turn)
   (let* ([from (get-player-location turn)]
          [to (move-fn from)])
     (cond
-      ((valid-horizontal-move (gamestate-current-map turn)
+      ((valid-move (gamestate-current-map turn)
                               from
                               to)
        (move-updated turn to))
       (else (move-failed turn)))))
+
 
 (define (move fn increment)
   (lambda (turn)
     (move-player (lambda (from)
                    (fn from increment))
                  turn)))
+
 
 (define move-left (move - 1))
 (define move-right (move + 1))
